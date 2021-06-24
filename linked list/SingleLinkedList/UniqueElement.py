2fd330e1dcee4575c0702b9c8363066ee628b7dd
radeep <46672838+radeeplucky143@users.noreply.github.com>
2021-06-24 12:47:37 +0530

    Update UniqueElement.py

--- linked list/SingleLinkedList/UniqueElement.py
+++ linked list/SingleLinkedList/UniqueElement.py
@@ -4,6 +4,10 @@ from os.path import dirname, abspath
 sys.path.append(dirname(abspath(__file__)))
 from SingleLinkedList import SingleLinkedList
 
+"""
+     Finding the unique Element in Single Linked List 
+"""
+
 
 def find_unique_element(linked_list):
         unique_value = 0
