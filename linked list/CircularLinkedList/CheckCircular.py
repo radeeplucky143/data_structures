941976ad643486f86ea5af72343dc89909aac2ce
radeep <46672838+radeeplucky143@users.noreply.github.com>
2021-06-24 12:51:07 +0530

    Update CheckCircular.py

--- linked list/CircularLinkedList/CheckCircular.py
+++ linked list/CircularLinkedList/CheckCircular.py
@@ -5,6 +5,11 @@ sys.path.append(dirname(dirname(abspath(__file__))))
 from CircularLinkedList.CircularLinkedList import CircularLinkedList
 from SingleLinkedList.SingleLinkedList import SingleLinkedList
 
+"""
+    Checking whether the linked listis circular or not
+
+"""
+
 def check_circular(linked_list):
     if linked_list. head:
         temp_node = linked_list.head
@@ -18,14 +23,5 @@ def check_circular(linked_list):
     print("Linked List was Empty")
 
 
-nodes = int(input("Enter the number of nodes: "))
-circular_linked_list = CircularLinkedList()
-single_linked_list = SingleLinkedList()
-for _ in range(nodes):
-    data = int(input("Enter the data : "))
-    circular_linked_list.insert_front(data)
-    single_linked_list.insert_front(data)
 
 
-check_circular(circular_linked_list)
-check_circular(single_linked_list)
