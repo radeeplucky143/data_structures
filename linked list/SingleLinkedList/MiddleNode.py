0edae036215647a68d387ca9b29a977a12e31a43
radeep <46672838+radeeplucky143@users.noreply.github.com>
2021-06-24 12:44:15 +0530

    Update MiddleNode.py

--- linked list/SingleLinkedList/MiddleNode.py
+++ linked list/SingleLinkedList/MiddleNode.py
@@ -4,6 +4,12 @@ from os.path import dirname, abspath
 sys.path.append(dirname(abspath(__file__)))
 from SingleLinkedList import SingleLinkedList
 
+"""
+      For the even number of nodes in a linked list we can select any one of the two middle nodes
+      depending upon the condition use any one of the two functions.
+
+"""
+
 def get_second_middle_node(linked_list):
         first_ptr = linked_list.head
         second_ptr = linked_list.head
