a96430025cd4fb8fa91513303e0326e5cac57560
RADEEP <ryenni@cisco.com>
2021-06-24 14:46:43 +0530

    Added ArrayStack data structure

---
+++ Stack/ArrayStack.py
@@ -0,0 +1,33 @@
+class ArrayStack:
+
+    def __init__(self):
+        self.items = list()
+
+
+    def push(self,data):
+        self.items.append(data)
+
+
+    def pop(self):
+        if self.items:
+            del self.items[-1]
+        else:
+            print("Stack was Empty")
+
+
+    def peek(self):
+        if self.items:
+            print("Top Element: {}".format(self.items[-1]))
+
+
+    def isEmpty(self):
+        if self.items:
+            print("not Empty")
+            return
+        print("Empty")
+
+
+    def print_stack(self):
+        for ele in self.items:
+            print(ele, end=' ')
+        print(end='\n')
