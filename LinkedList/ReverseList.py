4579080ce82c826de33636434db54b91ea1be159
RADEEP <ryenni@cisco.com>
2021-06-26 15:01:25 +0530

    Reverse_list of any type

--- LinkedList/ReverseList.py
+++ LinkedList/ReverseList.py
@@ -2,17 +2,38 @@ import sys
 from os.path import dirname, abspath
 
 sys.path.append(dirname(abspath(__file__)))
-from SingleLinkedList import SingleLinkedList
+from SingleLinkedList.SingleLinkedList import SingleLinkedList
+from CircularLinkedList.CircularLinkedList import CircularLinkedList
+from DoublyLinkedList.DoublyLinkedList import DoublyLinkedList
 
 """
-      This program is used to reverse the single linked list
+      This program is used to reverse the linked list of any type 
 """
 
 
-def reverse_list(single_linked_list):
-    reverse_linked_list = SingleLinkedList()
-    temp_node = single_linked_list.head
-    while temp_node:
-        reverse_linked_list.insert_front(temp_node.data)
-        temp_node = temp_node.next
-    return reverse_linked_list
+def reverse_list(linked_list):
+    if isinstance(linked_list, SingleLinkedList):
+        reversed_list = SingleLinkedList()
+        temp_node = linked_list.head
+        while temp_node:
+            reversed_list.insert_back(temp_node.data)
+            temp_node = temp_node.next
+        return reversed_list
+    elif isinstance(linked_list, CircularLinkedList):
+        reversed_list = CircularLinkedList()
+        temp_node = linked_list.head
+        while temp_node:
+            reversed_list.insert_back(temp_node.data)
+            temp_node = temp_node.next
+            if temp_node.next == linked_list.head:
+                break
+        return reversed_list
+    elif isinstance(linked_list, DoublyLinkedList):
+        reversed_list = DoublyLinkedList()
+        temp_node = linked_list.head
+        while temp_node:
+            reversed_list.insert_back(temp_node.data)
+            temp_node = temp_node.next
+        return reversed_list
+    else:
+        print("Incorrect Arguement Passed")
