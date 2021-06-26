62bafbaa891eb4208f1dc26966984907af747505
RADEEP <ryenni@cisco.com>
2021-06-26 14:37:32 +0530

    Finding the Middle Node for Linear/Circular

--- LinkedList/MiddleNode.py
+++ LinkedList/MiddleNode.py
@@ -15,21 +15,25 @@ def get_second_middle_node(linked_list):
     if linked_list.head:
         first_ptr = linked_list.head
         second_ptr = linked_list.head
-        while second_ptr.next and second_ptr != linked_list.head:
+        while second_ptr.next and second_ptr.next != linked_list.head:
             first_ptr = first_ptr.next
             second_ptr = second_ptr.next
-            if second_ptr.next:
+            if second_ptr.next and second_ptr.next != linked_list.head:
                 second_ptr = second_ptr.next
         print(first_ptr.data)
+        return
     print("Linked List is Empty")
 
 
 def get_first_middle_node(linked_list):
-    first_ptr = linked_list.head
-    second_ptr = linked_list.head
-    while second_ptr.next:
-        second_ptr = second_ptr.next
-        if second_ptr.next:
-            first_ptr = first_ptr.next
+    if linked_list.head:
+        first_ptr = linked_list.head
+        second_ptr = linked_list.head
+        while second_ptr.next and second_ptr.next != linked_list.head:
             second_ptr = second_ptr.next
-    print(first_ptr.data)
+            if second_ptr.next and second_ptr.next != linked_list.head:
+                first_ptr = first_ptr.next
+                second_ptr = second_ptr.next
+        print(first_ptr.data)
+        return
+    print("Linked List was Empty")
